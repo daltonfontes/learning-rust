2ff635c042ebe8273ce72566f712b6d2f02583ea
Dalton Fontes <fontes.dalton@hotmail.com>
2024-02-09 00:35:22 +0000

    feat : add remove task and list task

--- todo-list/src/main.rs
+++ todo-list/src/main.rs
@@ -1,30 +1,66 @@
-use std::io;
+use std::io::{self, Write};
 
 struct Task {
     name: String,
     status: bool,
 }
 
-fn create_task(list: &mut Vec<Task>) {
-    let mut task_name: String = String::new();
-    io::stdin()
-        .read_line(&mut task_name)
-        .expect("cannot readline");
-
-    let task: Task = Task {
-        name: task_name,
+fn create_task(task_list: &mut Vec<Task>, task_name: String) {
+    let new_task: Task = Task {
+        name: task_name.clone(),
         status: false,
     };
 
-    list.push(task);
+    task_list.push(new_task);
+}
+
+fn list_task(task_list: &Vec<Task>) {
+    println!("Task List:");
+    for task in task_list {
+        let status_str = if task.status {
+            "Concluída"
+        } else {
+            "Pendente"
+        };
+        println!("{} - {}, ", task.name, status_str)
+    }
+}
+
+fn remove_task(task_list: &Vec<Task>) {
+ task.retain(|task| task.name != task.name);
 }
 
 fn main() {
     let mut task_list: Vec<Task> = Vec::new();
 
-    create_task(&mut task_list);
+    loop {
+        println!("\nMenu:");
+        println!("1. Add Task");
+        println!("2. List Task");
+        print!("Enter your choice: ");
 
-    for task in &task_list {
-        println!("Name : {}, Status : {}", task.name, task.status);
+        io::stdout().flush().unwrap();
+
+        let mut input: String = String::new();
+        io::stdin().read_line(&mut input).expect("cannot readline");
+
+        match input.trim() {
+            "1" => {
+                print!("Enter task description:");
+                io::stdout().flush().unwrap();
+                let mut name: String = String::new();
+                io::stdin().read_line(&mut name).expect("cannot readline");
+
+                create_task(&mut task_list, name.trim().to_string());
+            }
+
+            "2" => {
+                list_task(&mut task_list);
+            }
+
+            _ => {
+                println!("Wrong input.");
+            }
+        }
     }
 }
